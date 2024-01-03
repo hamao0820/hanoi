db7202fd8ac677a7497d8ae844e32fc6d785a4da
浜田　俊祐 <hamashun0820@icloud.com>
2024-01-04 00:51:15 +0900

    add: Add mouse input handling for moving disks between towers

--- hanoi/hanoi.go
+++ hanoi/hanoi.go
@@ -1,6 +1,9 @@
 package hanoi
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
 
 /*
 Tower構造体とDisk構造体のsliceをセットで扱うための構造体
@@ -26,6 +29,34 @@ func (h *Hanoi) Draw(screen *ebiten.Image) {
 	for i, d := range h.disks {
 		d.Draw(screen, h.id*200+100-d.width/2+TowerWidth/2, ScreenHeight-(i+1)*DiskHeight-StandHeight)
 	}
+}
+
+func (h *Hanoi) Top() *Disk {
+	if len(h.disks) == 0 {
+		return nil
+	}
+	return h.disks[len(h.disks)-1]
+}
+
+func (h *Hanoi) Pop() *Disk {
+	if len(h.disks) == 0 {
+		return nil
+	}
+	d := h.disks[len(h.disks)-1]
+	h.disks = h.disks[:len(h.disks)-1]
+	return d
+}
+
+func (h *Hanoi) Push(d *Disk) {
+	h.disks = append(h.disks, d)
+}
+
+func (h *Hanoi) IsEmpty(x, y int) bool {
+	return len(h.disks) == 0
+}
+
+func (h *Hanoi) CanPush(d *Disk) bool {
+	return h.IsEmpty(0, 0) || h.Top().size > d.size
 }
 
 type Game struct {
@@ -54,6 +85,41 @@ func NewGame() *Game {
 }
 
 func (g *Game) Update() error {
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		var d *Disk
+		var c *Hanoi
+		for _, h := range g.honoi {
+			if h.tower.IsAround(x, y) {
+				if !h.IsEmpty(x, y) {
+					d = h.Top()
+					c = h
+					break
+				}
+			}
+		}
+
+		if d == nil {
+			return nil
+		}
+
+		var n *Hanoi
+		for _, h := range g.honoi {
+			if h.CanPush(d) {
+				n = h
+				break
+			}
+		}
+
+		if n == nil {
+			return nil
+		}
+
+		c.Pop()
+		n.Push(d)
+
+	}
+
 	return nil
 }
 
