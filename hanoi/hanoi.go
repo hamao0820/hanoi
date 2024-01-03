ff28226d92c475872d16f1e073d8ca6345874d83
浜田　俊祐 <hamashun0820@icloud.com>
2024-01-04 00:30:37 +0900

    add: Add Hanoi struct implement drawing functionality

--- hanoi/hanoi.go
+++ hanoi/hanoi.go
@@ -2,7 +2,36 @@ package hanoi
 
 import "github.com/hajimehoshi/ebiten/v2"
 
-type Game struct{}
+/*
+Tower構造体とDisk構造体のsliceをセットで扱うための構造体
+idは0, 1, 2のいずれか
+disksは大きい順に並んでいるものとする
+*/
+type Hanoi struct {
+	id    int
+	tower *Tower
+	disks []*Disk
+}
+
+func NewHanoi(id int) *Hanoi {
+	return &Hanoi{
+		id:    id,
+		tower: NewTower(),
+		disks: []*Disk{},
+	}
+}
+
+func (h *Hanoi) Draw(screen *ebiten.Image) {
+	h.tower.Draw(screen, h.id*200+100, ScreenHeight-TowerHeight)
+	for i, d := range h.disks {
+		d.Draw(screen, h.id*200+100-d.width/2+TowerWidth/2, ScreenHeight-(i+1)*DiskHeight-StandHeight)
+	}
+}
+
+type Game struct {
+	honoi [3]*Hanoi
+	stand *Stand
+}
 
 const (
 	ScreenWidth  = 640
@@ -10,14 +39,30 @@ const (
 )
 
 func NewGame() *Game {
-	return &Game{}
+	h1 := NewHanoi(0)
+	h1.disks = append(h1.disks, NewDisk(3))
+	h1.disks = append(h1.disks, NewDisk(2))
+	h1.disks = append(h1.disks, NewDisk(1))
+	return &Game{
+		honoi: [3]*Hanoi{
+			h1,
+			NewHanoi(1),
+			NewHanoi(2),
+		},
+		stand: NewStand(),
+	}
 }
 
 func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {}
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.stand.Draw(screen)
+	for _, h := range g.honoi {
+		h.Draw(screen)
+	}
+}
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return ScreenWidth, ScreenHeight
