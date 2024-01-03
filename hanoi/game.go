62972b6a4166554f6c13ee4db5f563274350ec77
浜田　俊祐 <hamashun0820@icloud.com>
2024-01-04 01:09:45 +0900

    feat: クリックで移動先を選択できるようにした

--- hanoi/game.go
+++ hanoi/game.go
@@ -1,13 +1,16 @@
 package hanoi
 
 import (
+	"fmt"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Game struct {
-	honoi [3]*Hanoi
-	stand *Stand
+	honoi    [3]*Hanoi
+	stand    *Stand
+	selected *Hanoi
 }
 
 const (
@@ -33,37 +36,25 @@ func NewGame() *Game {
 func (g *Game) Update() error {
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
-		var d *Disk
-		var c *Hanoi
 		for _, h := range g.honoi {
 			if h.tower.IsAround(x, y) {
-				if !h.IsEmpty(x, y) {
-					d = h.Top()
-					c = h
-					break
+				switch g.selected {
+				case nil:
+					if !h.IsEmpty(x, y) {
+						g.selected = h
+					}
+				case h:
+					g.selected = nil
+				default:
+					fmt.Println("push")
+					if h.CanPush(g.selected.Top()) {
+						h.Push(g.selected.Pop())
+						g.selected = nil
+					}
 				}
-			}
-		}
-
-		if d == nil {
-			return nil
-		}
-
-		var n *Hanoi
-		for _, h := range g.honoi {
-			if h.CanPush(d) {
-				n = h
 				break
 			}
 		}
-
-		if n == nil {
-			return nil
-		}
-
-		c.Pop()
-		n.Push(d)
-
 	}
 
 	return nil
@@ -74,6 +65,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for _, h := range g.honoi {
 		h.Draw(screen)
 	}
+
+	if g.selected != nil {
+		x, y := ebiten.CursorPosition()
+		g.selected.Top().Draw(screen, x-g.selected.Top().width/2, y-DiskHeight/2)
+	}
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
